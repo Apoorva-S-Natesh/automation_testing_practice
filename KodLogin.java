fcc9edef2cbfe4bea2be508171288f29d42739ac
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-04 12:40:19 +0200

    WebElement interface methods
    getLocation, getSize, getAttribute, getTagName, sendKeys

--- KodLogin.java
+++ KodLogin.java
@@ -1,6 +1,8 @@
 package locator;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -22,6 +24,17 @@ public class KodLogin {
 				passwordField.sendKeys("1234");
 				WebElement loginButton = driver.findElement(By.id("login-submit-button"));
 				loginButton.click();
+				
+				//Storing and printing various properties of the email input field
+				String tagName = emailField.getTagName(); //Retrieves the tag name
+				String attributeValue = emailField.getAttribute("name");
+				Dimension size = emailField.getSize();
+				Point location = emailField.getLocation();
+				
+				System.out.println("Tag name of email field" + tagName);
+				System.out.println(attributeValue);
+				System.out.println(size.width + " and " + size.height);
+				System.out.println(location.x + " and " + location.y);
 				
 				/*
 				driver.findElement(By.name(null));
