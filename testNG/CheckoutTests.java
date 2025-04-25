16d9b91c0370f7cf68e661908ab9ec4493045058
Apoorva-S-Natesh <104731101+Apoorva-S-Natesh@users.noreply.github.com>
2025-04-25 13:08:08 +0200

    learn: adding groups to test and before, after methods

--- testNG/CheckoutTests.java
+++ testNG/CheckoutTests.java
@@ -6,43 +6,40 @@ import org.testng.annotations.Test;
 
 public class CheckoutTests {
 	
-	@BeforeSuite
+	@BeforeSuite (groups = "smoke") //If groups for these methods are not provided then they wont be executed
 	public void setupSuite() {
 		System.out.println("Setting up the test suite: Initializing shared resources.");
 	}
 	
-	@Test (groups = "smoke")
-	//(priority = 1)
+	@Test (groups = "smoke", priority = 1)
 	public void testCheckoutWithCreditCard() {
 		System.out.println("Validate that a user is able to succesfully complete checkout using a credit card.");	
 	}
 	
-	@Test(groups = "regression")
-	//(priority = 2)
+	@Test(groups = "regression", priority = 2)
 	public void testcheckoutWithUPI() {
 			System.out.println("Validate that a user is able to succesfully complete checkout using UPI.");
 	}
 	
-	@Test (groups = "smoke")
-	//(priority = 3)
-	public void testcheckoutWithCashOnDElivery() {
+	@Test (groups = "smoke", priority = 3)
+	public void testcheckoutWithCashOnDelivery() {
 		System.out.println("Validate that a user is able to select 'Cash on Delivery' as a payment option.");
 	}
 	
 	@Test
-	//(priority = 4)
+	(priority = 4, enabled=false) // test is disabled, its not executed and ignored in testNG report
 	public void testcheckoutWithExpiredCard() {
 		System.out.println("Validate that the system correctly displays an error message when an expired card is used for checkout.");
 	}
 	
 	@Test
-	//(priority = 5)
+	(priority = 5)
 	public void testApplyingDiscountCode() {
 		System.out.println("Validate that the user can successfully apply a avalid discount code at checkout");
 	}
 	
 	// runs once after all the test cases in the test folder, not counted as a test case. Cannot have priority
-	@AfterTest
+	@AfterTest (groups = "regression")
 	public void cleanup() {
 		System.out.println("Logging out user and closing browser session -@AfterTest");
 	}
